85665dbe49bf7cdd9475b21ed55b1515042d9d07
KristianWede <kris79s4@stud.kea.dk>
2022-05-11 10:14:44 +0200

    Changes by Rami

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
 
+//rami har lavet lavet main class
+
     public static void main(String[] args) {
         Main main = new Main();
         main.execute();
