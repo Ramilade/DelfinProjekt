8f655fd9a0433b44a6714eb0df189d092e87d89e
KristianWede <kris79s4@stud.kea.dk>
2022-05-11 10:15:13 +0200

    Changes by Bjørn

--- src/filhåndtering/Database.java
+++ src/filhåndtering/Database.java
@@ -1,7 +1,17 @@
 package filhåndtering;
 
+import com.company.Member;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Database {
-    public void createMember(){
+    public void createMember() throws FileNotFoundException {
+        Member bob = new Member("Bob",29);
+        PrintStream out = new PrintStream("Svømmeclub.csv");
+        out.print(bob.getName());
+        out.print(";");
+        out.print(bob.getAge());
 
     }
 }
