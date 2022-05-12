40f90aa6adedfa43c4e725c0472dac22596a438a
Mark <mark771e@stud.kea.dk>
2022-05-12 09:00:09 +0200

    Added get/set's for "Discipline" removed "iMember"

--- src/logic/Discipline.java
+++ src/logic/Discipline.java
@@ -14,5 +14,27 @@ public class Discipline {
   public Discipline() {
   }
 
+  public String getName() {
+    return name;
+  }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getDistance() {
+    return distance;
+  }
+
+  public void setDistance(int distance) {
+    this.distance = distance;
+  }
+
+  public double getRecord() {
+    return record;
+  }
+
+  public void setRecord(double record) {
+    this.record = record;
+  }
 }
--- src/logic/Member.java
+++ src/logic/Member.java
@@ -4,18 +4,21 @@ public class Member {
 
   private String name;
   private int age;
+  private boolean active;
+  private int subscription;
+  private Discipline crawl;
+  private Discipline backCrawl;
+  private Discipline breastStroke;
+  private Discipline butterfly;
 
   public Member(String name, int age, boolean active) {
     this.name = name;
     this.age = age;
     this.active = active;
-
-    /*
     this.crawl = new Discipline();
     this.backCrawl = new Discipline();
     this.breastStroke = new Discipline();
     this.butterfly = new Discipline();
-    * */
   }
 
   public Member(String name, int age) {
@@ -46,13 +49,6 @@ public class Member {
   public void setActive(boolean active) {
     this.active = active;
   }
-
-  private boolean active;
-  private int subscription;
-  private Discipline crawl;
-  private Discipline backCrawl;
-  private Discipline breastStroke;
-  private Discipline butterfly;
 
 
 }
