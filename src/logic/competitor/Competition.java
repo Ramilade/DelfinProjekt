f5e38250b33bd752af610ce54f242dd4629569d0
Mark <mark771e@stud.kea.dk>
2022-05-19 11:53:31 +0200

    filled out Competition.java

--- src/logic/competitor/Competition.java
+++ src/logic/competitor/Competition.java
@@ -1,8 +1,51 @@
 package logic.competitor;
 
+import java.time.LocalDate;
+
 public class Competition {
 
+    private String place;
+    private int ranking;
+    private LocalDate date;
+    private double time;
 
 
+    public Competition(String place, int ranking, LocalDate date, double time) {
+        this.place = place;
+        this.ranking = ranking;
+        this.date = date;
+        this.time = time;
+    }
 
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
 }
