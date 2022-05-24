9623344027e71b4646df0bb53ce8b369b47724cc
Mark <mark771e@stud.kea.dk>
2022-05-24 14:21:11 +0200

    Added RankingGroup.java

---
+++ src/logic/competitor/RankingGroup.java
@@ -0,0 +1,24 @@
+package logic.competitor;
+
+public enum RankingGroup {
+    SENIOR_CRAWL,
+    SENIOR_RYGCRAWL,
+    SENIOR_BRYSTSVØMNING,
+    SENIOR_BUTTERFLY,
+    JUNIOR_CRAWL,
+    JUNIOR_RYGCRAWL,
+    JUNIOR_BRYSTSVØMNING,
+    JUNIOR_BUTTERFLY;
+
+    public DisciplineType translateToDisciplineType() {
+        DisciplineType type;
+        switch (this) {
+            case JUNIOR_CRAWL,SENIOR_CRAWL -> type = DisciplineType.CRAWL;
+            case JUNIOR_BRYSTSVØMNING,SENIOR_BRYSTSVØMNING -> type = DisciplineType.BRYSTSWØMNING;
+            case JUNIOR_RYGCRAWL,SENIOR_RYGCRAWL -> type = DisciplineType.RYGCRAWL;
+            case JUNIOR_BUTTERFLY,SENIOR_BUTTERFLY -> type = DisciplineType.BUTTERFLY;
+            default -> type = null;
+        }
+        return type;
+    }
+}
