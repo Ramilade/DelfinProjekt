d5135dd3ac43b014974d9b2cbecab847b41934cd
Mark <mark771e@stud.kea.dk>
2022-05-25 14:29:35 +0200

    Added MemberMismatchException.java Added MemberStatusMismatchException.java Added MemberTypeMismatchException.java
    exceptionhandling on adding a member in controller

---
+++ src/logic/exceptions/MemberTypeMismatchException.java
@@ -0,0 +1,7 @@
+package logic.exceptions;
+
+public class MemberTypeMismatchException extends MemberMismatchException{
+    public MemberTypeMismatchException(String s) {
+        super(s);
+    }
+}
